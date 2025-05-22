520a9fe5e8f9d134ff4d15354c044d1157d5b535
skyeWithAnE23 <gavin.dunn2030@gmail.com>
2025-05-22 08:35:30 -0400

    Create baseInstall.sh

---
+++ baseInstall.sh
@@ -0,0 +1,3 @@
+update #Aliesed as sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y (see .bashrc)
+
+sudo apt install kde-plasma-desktop i3 flatpack dolphin konsole git python3 tmux net-tools docker hashcat neovim
